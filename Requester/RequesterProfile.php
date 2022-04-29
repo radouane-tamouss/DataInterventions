e49747ff724ab569997aa4a224e61462806fd01a
TAMOUSS Radouane <atamousse.red@gmail.com>
2022-04-29 06:22:49 +0000

    add files

---
+++ Requester/RequesterProfile.php
@@ -0,0 +1,221 @@
+<?php
+define('TITLE', 'Requester Profile');
+define('PAGE', 'RequesterProfile');
+include('includes/header.php'); 
+include('../dbConnection.php');
+ session_start();
+ if($_SESSION['is_login']){
+  $rEmail = $_SESSION['rEmail'];
+ } else {
+  echo "<script> location.href='RequesterLogin.php'; </script>";
+ }
+
+ $sql = "SELECT * FROM requesterlogin_tb WHERE r_email='$rEmail'";
+ $result = $conn->query($sql);
+ if($result->num_rows == 1){
+ $row = $result->fetch_assoc();
+ $rName = $row["r_name"]; }
+
+ if(isset($_REQUEST['nameupdate'])){
+  if(($_REQUEST['rName'] == "")){
+   // msg displayed if required field missing
+   $passmsg = '<div class="alert alert-warning col-sm-6 ml-5 mt-2" role="alert"> Fill All Fileds </div>';
+  } else {
+   $rName = $_REQUEST["rName"];
+   $sql = "UPDATE requesterlogin_tb SET r_name = '$rName' WHERE r_email = '$rEmail'";
+   if($conn->query($sql) == TRUE){
+   // below msg display on form submit success
+   $passmsg = '<div class="alert alert-success col-sm-6 ml-5 mt-2" role="alert"> Updated Successfully </div>';
+   } else {
+   // below msg display on form submit failed
+   $passmsg = '<div class="alert alert-danger col-sm-6 ml-5 mt-2" role="alert"> Unable to Update </div>';
+      }
+    }
+   }
+?>
+
+<div class="layout-page">
+  <!-- Navbar -->
+
+  <nav
+    class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
+    id="layout-navbar"
+  >
+    <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
+      <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
+        <i class="bx bx-menu bx-sm"></i>
+      </a>
+    </div>
+
+    <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+      <!-- Search -->
+      <div class="navbar-nav align-items-center">
+        <div class="nav-item d-flex align-items-center">
+          <i class="bx bx-search fs-4 lh-0"></i>
+          <input
+            type="text"
+            class="form-control border-0 shadow-none"
+            placeholder="Search..."
+            aria-label="Search..."
+          />
+        </div>
+      </div>
+      <!-- /Search -->
+
+      <ul class="navbar-nav flex-row align-items-center ms-auto">
+        <!-- Place this tag where you want the button to render. -->
+        <li class="nav-item lh-1 me-3">
+          <a
+            class="github-button"
+            href="https://github.com/themeselection/sneat-html-admin-template-free"
+            data-icon="octicon-star"
+            data-size="large"
+            data-show-count="true"
+            aria-label="Star themeselection/sneat-html-admin-template-free on GitHub"
+            >Star</a
+          >
+        </li>
+
+        <!-- User -->
+        <li class="nav-item navbar-dropdown dropdown-user dropdown">
+          <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
+            <div class="avatar avatar-online">
+              <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+            </div>
+          </a>
+          <ul class="dropdown-menu dropdown-menu-end">
+            <li>
+              <a class="dropdown-item" href="#">
+                <div class="d-flex">
+                  <div class="flex-shrink-0 me-3">
+                    <div class="avatar avatar-online">
+                      <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+                    </div>
+                  </div>
+                  <div class="flex-grow-1">
+                    <span class="fw-semibold d-block"><?php echo $rName?></span>
+                    <small class="text-muted">Admin</small>
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li>
+              <div class="dropdown-divider"></div>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">
+                <i class="bx bx-user me-2"></i>
+                <span class="align-middle">My Profile</span>
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">
+                <i class="bx bx-cog me-2"></i>
+                <span class="align-middle">Settings</span>
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">
+                <span class="d-flex align-items-center align-middle">
+                  <i class="flex-shrink-0 bx bx-credit-card me-2"></i>
+                  <span class="flex-grow-1 align-middle">Billing</span>
+                  <span class="flex-shrink-0 badge badge-center rounded-pill bg-danger w-px-20 h-px-20">4</span>
+                </span>
+              </a>
+            </li>
+            <li>
+              <div class="dropdown-divider"></div>
+            </li>
+            <li>
+              <a class="dropdown-item" href="auth-login-basic.html">
+                <i class="bx bx-power-off me-2"></i>
+                <span class="align-middle">Log Out</span>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <!--/ User -->
+      </ul>
+    </div>
+  </nav>
+
+  <!-- / Navbar -->
+
+  <!-- Content wrapper -->
+  <div class="content-wrapper">
+    <!-- Content -->
+
+    <div class="container-xxl flex-grow-1 container-p-y">
+      <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Forms/</span> Vertical Layouts</h4>
+
+      <!-- Basic Layout -->
+      <div class="row">
+     
+        <div class="col-xl">
+          <div class="card mb-4">
+            <div class="card-header d-flex justify-content-between align-items-center">
+              <h5 class="mb-0">Basic with Icons</h5>
+              <small class="text-muted float-end">Merged input group</small>
+            </div>
+            <div class="card-body">
+              <form>
+                <div class="mb-3">
+                  <label class="form-label" for="basic-icon-default-fullname">Full Name</label>
+                  <div class="input-group input-group-merge">
+                    <span id="basic-icon-default-fullname2" class="input-group-text"
+                      ><i class="bx bx-user"></i
+                    ></span>
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="basic-icon-default-fullname"
+                      name="rName"
+                      value="<?php echo $rName?>"
+                      aria-describedby="basic-icon-default-fullname2"
+                    />
+                  </div>
+                </div>
+                
+                
+                <div class="mb-3">
+                  <label class="form-label" for="basic-icon-default-email">Email</label>
+                  <div class="input-group input-group-merge">
+                    <span class="input-group-text"><i class="bx bx-envelope"></i></span>
+                    <input
+                      type="text"
+                      id="basic-icon-default-email"
+                      class="form-control"
+                      value=" <?php echo $rEmail ?>"
+                      readonly
+                     
+                      
+                    />
+                    
+                  </div>
+                 
+                </div>
+                
+               
+                <button type="submit" class="btn btn-primary" name="nameupdate">Update</button>
+                <?php if(isset($passmsg)) {echo $passmsg; } ?>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- / Content -->
+
+   
+
+    <div class="content-backdrop fade"></div>
+  </div>
+  <!-- Content wrapper -->
+</div>
+
+
+
+
+<?php
+include('includes/footer.php'); 
+?>
---
+++ Requester/includes/header.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <title>
+  <?php echo TITLE ?>
+ </title>
+
+
+ <link rel="icon" type="image/x-icon" href="../assets/img/favicon/favicon.ico" />
+
+<!-- Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link
+  href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+  rel="stylesheet"
+/>
+
+<!-- Icons. Uncomment required icon fonts -->
+<link rel="stylesheet" href="../assets/vendor/fonts/boxicons.css" />
+
+<!-- Core CSS -->
+<link rel="stylesheet" href="../assets/vendor/css/core.css" class="template-customizer-core-css" />
+<link rel="stylesheet" href="../assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+<link rel="stylesheet" href="../assets/css/demo.css" />
+
+<!-- Vendors CSS -->
+<link rel="stylesheet" href="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+
+<link rel="stylesheet" href="../assets/vendor/libs/apex-charts/apex-charts.css" />
+
+<!-- Page CSS -->
+
+<!-- Helpers -->
+<script src="../assets/vendor/js/helpers.js"></script>
+
+<!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+<!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+<script src="../assets/js/config.js"></script>
+</head>
+
+<body>
+ <!-- Top Navbar -->
+
+
+ <!-- Side Bar -->
+ <div class="layout-wrapper layout-content-navbar">
+      <div class="layout-container">
+        <!-- Menu -->
+  <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+        
+            
+
+        <div class="menu-inner-shadow"></div>
+       
+        <ul class="menu-inner py-1">
+          <!-- Dashboard -->
+          
+          <li class="menu-item <?php if(PAGE == 'RequesterProfile') { echo 'active'; } ?>">
+            <a href="RequesterProfile.php" class="menu-link ">
+              <i class="menu-icon tf-icons bx bx-home-circle"></i>
+              <div data-i18n="Analytics">profile</div>
+            </a>
+          </li>
+          <li class="menu-item <?php if(PAGE == 'SubmitRequest') { echo 'active'; } ?>">
+              <a href="SubmitRequest.php" class="menu-link ">
+                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <div data-i18n="Analytics">submit request</div>
+              </a>
+          </li>
+          <li class="menu-item <?php if(PAGE == 'CheckStatus') { echo 'active'; } ?>">
+              <a href="CheckStatus.php" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <div data-i18n="Analytics">service status</div>
+              </a>
+          </li>
+          <li class="menu-item <?php if(PAGE == 'Requesterchangepass') { echo 'active'; } ?>">
+              <a href="Requesterchangepass.php" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <div data-i18n="Analytics">change password</div>
+              </a>
+          </li>
+          <li class="menu-item">
+              <a href="../logout.php" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <div data-i18n="Analytics">logout</div>
+              </a>
+          </li>
+        </ul>
+      </aside>
+
+
+
+
+
