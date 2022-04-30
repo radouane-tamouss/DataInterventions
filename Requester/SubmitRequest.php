3a68481c07340af4339874a15310bca2c5afdb25
TAMOUSS Radouane <atamousse.red@gmail.com>
2022-04-30 06:46:02 +0000

    add SubmitRequest

---
+++ Requester/SubmitRequest.php
@@ -0,0 +1,294 @@
+<?php
+define('TITLE', 'Submit Request');
+define('PAGE', 'SubmitRequest');
+include('includes/header.php'); 
+include('../dbConnection.php');
+session_start();
+if($_SESSION['is_login']){
+ $rEmail = $_SESSION['rEmail'];
+} else {
+ echo "<script> location.href='RequesterLogin.php'; </script>";
+}
+if(isset($_REQUEST['submitrequest'])){
+ // Checking for Empty Fields
+ if(($_REQUEST['requestinfo'] == "") || ($_REQUEST['requestdesc'] == "") || ($_REQUEST['requestername'] == "") || ($_REQUEST['requesteradd1'] == "") || ($_REQUEST['requesteradd2'] == "") || ($_REQUEST['requestercity'] == "") || ($_REQUEST['requesterstate'] == "") || ($_REQUEST['requesterzip'] == "") || ($_REQUEST['requesteremail'] == "") || ($_REQUEST['requestermobile'] == "") || ($_REQUEST['requestdate'] == "")){
+  // msg displayed if required field missing
+  $msg = '<div class="alert alert-warning col-sm-6 ml-5 mt-2" role="alert"> Fill All Fileds </div>';
+ } else {
+   // Assigning User Values to Variable
+   $rinfo = $_REQUEST['requestinfo'];
+   $rdesc = $_REQUEST['requestdesc'];
+   $rname = $_REQUEST['requestername'];
+   $radd1 = $_REQUEST['requesteradd1'];
+   $radd2 = $_REQUEST['requesteradd2'];
+   $rcity = $_REQUEST['requestercity'];
+   $rstate = $_REQUEST['requesterstate'];
+   $rzip = $_REQUEST['requesterzip'];
+   $remail = $_REQUEST['requesteremail'];
+   $rmobile = $_REQUEST['requestermobile'];
+   $rdate = $_REQUEST['requestdate'];
+   $sql = "INSERT INTO submitrequest_tb(request_info, request_desc, requester_name, requester_add1, requester_add2, requester_city, requester_state, requester_zip, requester_email, requester_mobile, request_date) VALUES ('$rinfo','$rdesc', '$rname', '$radd1', '$radd2', '$rcity', '$rstate', '$rzip', '$remail', '$rmobile', '$rdate')";
+   if($conn->query($sql) == TRUE){
+    // below msg display on form submit success
+    $genid = mysqli_insert_id($conn);
+    $msg = '<div class="alert alert-success col-sm-6 ml-5 mt-2" role="alert"> Request Submitted Successfully Your' . $genid .' </div>';
+    session_start();
+    $_SESSION['myid'] = $genid;
+    echo "<script> location.href='submitrequestsuccess.php'; </script>";
+    include('submitrequestsuccess.php');
+   } else {
+    // below msg display on form submit failed
+    $msg = '<div class="alert alert-danger col-sm-6 ml-5 mt-2" role="alert"> Unable to Submit Your Request </div>';
+   }
+ }
+}
+?>
+
+<div class="layout-page">
+  <!-- Navbar -->
+
+  <nav
+    class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
+    id="layout-navbar"
+  >
+    <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
+      <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
+        <i class="bx bx-menu bx-sm"></i>
+      </a>
+    </div>
+
+    <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+      <!-- Search -->
+      <div class="navbar-nav align-items-center">
+        <div class="nav-item d-flex align-items-center">
+          <i class="bx bx-search fs-4 lh-0"></i>
+          <input
+            type="text"
+            class="form-control border-0 shadow-none"
+            placeholder="Search..."
+            aria-label="Search..."
+          />
+        </div>
+      </div>
+      <!-- /Search -->
+
+      <ul class="navbar-nav flex-row align-items-center ms-auto">
+        <!-- Place this tag where you want the button to render. -->
+        <li class="nav-item lh-1 me-3">
+          <a
+            class="github-button"
+            href="https://github.com/themeselection/sneat-html-admin-template-free"
+            data-icon="octicon-star"
+            data-size="large"
+            data-show-count="true"
+            aria-label="Star themeselection/sneat-html-admin-template-free on GitHub"
+            >Star</a
+          >
+        </li>
+
+        <!-- User -->
+        <li class="nav-item navbar-dropdown dropdown-user dropdown">
+          <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
+            <div class="avatar avatar-online">
+              <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+            </div>
+          </a>
+          <ul class="dropdown-menu dropdown-menu-end">
+            <li>
+              <a class="dropdown-item" href="#">
+                <div class="d-flex">
+                  <div class="flex-shrink-0 me-3">
+                    <div class="avatar avatar-online">
+                      <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+                    </div>
+                  </div>
+                  <div class="flex-grow-1">
+                    <span class="fw-semibold d-block"><?php echo $rName?></span>
+                    <small class="text-muted">Admin</small>
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li>
+              <div class="dropdown-divider"></div>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">
+                <i class="bx bx-user me-2"></i>
+                <span class="align-middle">My Profile</span>
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">
+                <i class="bx bx-cog me-2"></i>
+                <span class="align-middle">Settings</span>
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">
+                <span class="d-flex align-items-center align-middle">
+                  <i class="flex-shrink-0 bx bx-credit-card me-2"></i>
+                  <span class="flex-grow-1 align-middle">Billing</span>
+                  <span class="flex-shrink-0 badge badge-center rounded-pill bg-danger w-px-20 h-px-20">4</span>
+                </span>
+              </a>
+            </li>
+            <li>
+              <div class="dropdown-divider"></div>
+            </li>
+            <li>
+              <a class="dropdown-item" href="auth-login-basic.html">
+                <i class="bx bx-power-off me-2"></i>
+                <span class="align-middle">Log Out</span>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <!--/ User -->
+      </ul>
+    </div>
+  </nav>
+
+  <!-- / Navbar -->
+
+  <!-- Content wrapper -->
+  <div class="content-wrapper">
+    <!-- Content -->
+
+    <div class="container-xxl flex-grow-1 container-p-y">
+      <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Forms/</span> Vertical Layouts</h4>
+
+      <!-- Basic Layout -->
+      <div class="row">
+     
+        <div class="col-xl">
+          <div class="card mb-4">
+            <div class="card-header d-flex justify-content-between align-items-center">
+              <h5 class="mb-0">Basic with Icons</h5>
+              <small class="text-muted float-end">Merged input group</small>
+            </div>
+            <div class="card-body">
+              <form  class="mx-5" action="" method="POST">
+               
+
+                <div class="row mb-3">
+                          <label class="col-sm-2 col-form-label" for="inputRequestInfo">requesst info</label>
+                          <div class="col-sm-10">
+                            <input
+                            id="inputRequestInfos"
+                            class="form-control form-control-lg"
+                            type="text"
+                            name="requestinfo"
+                            placeholder="Request Info"
+                            />
+                          </div>
+                </div>
+
+                <div class="row mb-3">
+                          <label class="col-sm-2 col-form-label" for="inputRequestDescription">Description</label>
+                          <div class="col-sm-10">
+                            <!-- <input type="text" class="form-control" id="basic-default-name" placeholder="Write Description" name="requestdesc"/> -->
+                            <textarea class="form-control" id="inputRequestDescription" rows="3" placeholder="Write Description" name="requestdesc"></textarea>
+
+                          </div>
+                </div>
+                <div class="row mb-3">
+                          <label class="col-sm-2 col-form-label" for="inputName">Name</label>
+                          <div class="col-sm-10">
+                            <input type="text" class="form-control" id="inputName" placeholder="Redouane" name="requestername"/>
+                          </div>
+                </div>
+                <div class="row mb-3">
+                          <label class="col-sm-2 col-form-label" for="inputEmail">Email</label>
+                          <div class="col-sm-10">
+                            <input type="text" class="form-control" id="inputEmail" placeholder="name@example.com" name="requesteremail"/>
+                          </div>
+                </div>
+                <div class="row mb-3">
+                          <label class="col-sm-2 col-form-label" for="inputMobile">Mobile</label>
+                          <div class="col-sm-10">
+                            <input type="text" class="form-control" id="inputMobile" placeholder="0666661010" name="requestermobile"  onkeypress="isInputNumber(event)"/>
+                          </div>
+                </div>
+                <div class="row mb-3">
+                          <label class="col-sm-2 col-form-label" for="inputDate">Date</label>
+                          <div class="col-sm-10">
+                          <input
+                          class="form-control"
+                          type="datetime-local"
+                          value="2022-04-28T12:30:00"
+                          id="inputDate"
+                          name="requestdate"
+
+                        />
+                          </div>
+                </div>
+                <div class="row mb-3">
+                          <label class="col-sm-2 col-form-label" for="inputAddress">Address Line 1</label>
+                          <div class="col-sm-10">
+                            <input type="text" class="form-control" id="inputAddress" placeholder="Address Line 1" name="requesteradd1"/>
+                          </div>
+                </div>
+                <div class="row mb-3">
+                          <label class="col-sm-2 col-form-label" for="inputAddress2">Adresse Line 2</label>
+                          <div class="col-sm-10">
+                            <input type="text" class="form-control" id="inputAddress2" placeholder="Address Line 2" name="requesteradd2"/>
+                          </div>
+                </div>
+                <div class="row mb-3">
+                          <label class="col-sm-2 col-form-label" for="inputCity">City</label>
+                          <div class="col-sm-10">
+                            <input type="text" class="form-control" id="inputCity" placeholder="City" name="requestercity"/>
+                          </div>
+                </div>
+                <div class="row mb-3">
+                          <label class="col-sm-2 col-form-label" for="inputState">province</label>
+                          <div class="col-sm-10">
+                            <input type="text" class="form-control" id="inputState" placeholder="privince" name="requesterstate"/>
+                          </div>
+                </div>
+                <div class="row mb-3">
+                          <label class="col-sm-2 col-form-label" for="inpotZip">Code Postale</label>
+                          <div class="col-sm-10">
+                            <input type="text" class="form-control" id="inputZip" placeholder="Code Postale" name="requesterzip" onkeypress="isInputNumber(event)"/>
+                          </div>
+                </div>
+
+                
+           
+               
+                
+                <button type="submit" class="btn btn-primary me-2" name="submitrequest">Submit</button>
+                <button type="reset" class="btn btn-outline-secondary">reset</button>
+
+                <?php if(isset($msg)) {echo $msg; } ?>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- / Content -->
+
+   
+
+    <div class="content-backdrop fade"></div>
+  </div>
+  <!-- Content wrapper -->
+</div>
+
+</div>
+</div>
+<!-- Only Number for input fields -->
+<script>
+  function isInputNumber(evt) {
+    var ch = String.fromCharCode(evt.which);
+    if (!(/[0-9]/.test(ch))) {
+      evt.preventDefault();
+    }
+  }
+</script>
+<?php
+include('includes/footer.php'); 
+$conn->close();
+?>
