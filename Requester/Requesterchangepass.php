b59733e8027529e653cd09fe92d5842638d7c440
TAMOUSS Radouane <atamousse.red@gmail.com>
2022-05-04 12:58:49 +0000

    update Requester/Requesterchangepass.php

--- Requester/Requesterchangepass.php
+++ Requester/Requesterchangepass.php
@@ -33,27 +33,189 @@ if($_SESSION['is_login']){
 }
 
 ?>
-<div class="col-sm-9 col-md-10">
-  <div class="row">
-    <div class="col-sm-6">
-      <form class="mt-5 mx-5" method="POST">
-        <div class="form-group">
-          <label for="inputEmail">Email</label>
-          <input type="email" class="form-control" id="inputEmail" value=" <?php echo $rEmail ?>" readonly>
-        </div>
-        <div class="form-group">
-          <label for="inputnewpassword">New Password</label>
-          <input type="password" class="form-control" id="inputnewpassword" placeholder="New Password" name="rPassword">
-        </div>
-        <button type="submit" class="btn btn-danger mr-4 mt-4" name="passupdate">Update</button>
-        <button type="reset" class="btn btn-secondary mt-4">Reset</button>
-        <?php if(isset($passmsg)) {echo $passmsg; } ?>
-      </form>
+<!-- ----------------------------------------------------------------------------------------------------------------------------- -->
 
+<div class="layout-page">
+  <!-- Navbar -->
+
+  <nav
+    class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
+    id="layout-navbar"
+  >
+    <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
+      <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
+        <i class="bx bx-menu bx-sm"></i>
+      </a>
     </div>
 
+    <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+      <!-- Search -->
+      <div class="navbar-nav align-items-center">
+        <div class="nav-item d-flex align-items-center">
+          <i class="bx bx-search fs-4 lh-0"></i>
+          <input
+            type="text"
+            class="form-control border-0 shadow-none"
+            placeholder="Search..."
+            aria-label="Search..."
+          />
+        </div>
+      </div>
+      <!-- /Search -->
+
+      <ul class="navbar-nav flex-row align-items-center ms-auto">
+        <!-- Place this tag where you want the button to render. -->
+        <li class="nav-item lh-1 me-3">
+          <a
+            class="github-button"
+            href="https://github.com/themeselection/sneat-html-admin-template-free"
+            data-icon="octicon-star"
+            data-size="large"
+            data-show-count="true"
+            aria-label="Star themeselection/sneat-html-admin-template-free on GitHub"
+            >Star</a
+          >
+        </li>
+
+        <!-- User -->
+        <li class="nav-item navbar-dropdown dropdown-user dropdown">
+          <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
+            <div class="avatar avatar-online">
+              <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+            </div>
+          </a>
+          <ul class="dropdown-menu dropdown-menu-end">
+            <li>
+              <a class="dropdown-item" href="#">
+                <div class="d-flex">
+                  <div class="flex-shrink-0 me-3">
+                    <div class="avatar avatar-online">
+                      <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+                    </div>
+                  </div>
+                  <div class="flex-grow-1">
+                    <span class="fw-semibold d-block"><?php echo $rName?></span>
+                    <small class="text-muted">Admin</small>
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li>
+              <div class="dropdown-divider"></div>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">
+                <i class="bx bx-user me-2"></i>
+                <span class="align-middle">My Profile</span>
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">
+                <i class="bx bx-cog me-2"></i>
+                <span class="align-middle">Settings</span>
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">
+                <span class="d-flex align-items-center align-middle">
+                  <i class="flex-shrink-0 bx bx-credit-card me-2"></i>
+                  <span class="flex-grow-1 align-middle">Billing</span>
+                  <span class="flex-shrink-0 badge badge-center rounded-pill bg-danger w-px-20 h-px-20">4</span>
+                </span>
+              </a>
+            </li>
+            <li>
+              <div class="dropdown-divider"></div>
+            </li>
+            <li>
+              <a class="dropdown-item" href="auth-login-basic.html">
+                <i class="bx bx-power-off me-2"></i>
+                <span class="align-middle">Log Out</span>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <!--/ User -->
+      </ul>
+    </div>
+  </nav>
+
+  <!-- / Navbar -->
+
+  <!-- Content wrapper -->
+  <div class="content-wrapper">
+    <!-- Content -->
+
+    <div class="container-xxl flex-grow-1 container-p-y">
+      <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Changer le mot de passe</span></h4>
+
+      <!-- Basic Layout -->
+      <div class="row">
+     
+        <div class="col-xl">
+          <div class="card mb-4">
+            
+            <div class="card-body">
+              <form method="POST">
+                <div class="mb-3">
+                  <label class="form-label" for="inputEmail">Email</label>
+                  <div class="input-group input-group-merge">
+                    <span id="basic-icon-default-fullname2" class="input-group-text"
+                      ><i class="bx bx-user"></i
+                    ></span>
+                    <input
+                      type="email"
+                      class="form-control"
+                      id="inputEmail"
+                      value="<?php echo $rEmail?>"
+                      aria-describedby="basic-icon-default-fullname2"
+                      readonly
+                    />
+                  </div>
+                </div>
+                
+                
+                <div class="mb-3">
+                  <label class="form-label" for="inputnewpassword">nouveau mot de passe</label>
+                  <div class="input-group input-group-merge">
+                    <span class="input-group-text"><i class="bx bx-envelope"></i></span>
+                    <input
+                      type="password"
+                      id="inputnewpassword"
+                      class="form-control"
+                      name="rPassword"
+                      placeholder="nouveau mot de passe"
+                     
+                      
+                    />
+                    
+                  </div>
+                 
+                </div>
+                
+                
+               
+                <button type="submit" class="btn btn-primary" name="passupdate">mettre à jour</button>
+                <button type="reset" class="btn btn-secondary" >réinitialiser</button>
+                <?php if(isset($passmsg)) {echo $passmsg; } ?>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- / Content -->
+
+   
+
+    <div class="content-backdrop fade"></div>
   </div>
+  <!-- Content wrapper -->
 </div>
+
+
+<!-- ----------------------------------------------------------------------------------------------------------------------------- -->
+
 </div>
 </div>
 
