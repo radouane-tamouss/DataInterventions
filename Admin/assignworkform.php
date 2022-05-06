d1b2dfdb997ebb4cdc69c2e855384ebf77eef125
TAMOUSS Radouane <atamousse.red@gmail.com>
2022-05-06 09:54:41 +0000

    add assignworkform.php

---
+++ Admin/assignworkform.php
@@ -0,0 +1,219 @@
+<?php    
+if(session_id() == '') {
+  session_start();
+}
+if(isset($_SESSION['is_adminlogin'])){
+ $aEmail = $_SESSION['aEmail'];
+} else {
+ echo "<script> location.href='login.php'; </script>";
+}
+ if(isset($_REQUEST['view'])){
+  $sql = "SELECT * FROM submitrequest_tb WHERE request_id = {$_REQUEST['id']}";
+ $result = $conn->query($sql);
+ $row = $result->fetch_assoc();
+ }
+
+ //  Assign work Order Request Data going to submit and save on db assignwork_tb table
+ if(isset($_REQUEST['assign'])){
+  // Checking for Empty Fields
+  if(($_REQUEST['request_id'] == "") || ($_REQUEST['request_info'] == "") || ($_REQUEST['requestdesc'] == "") || ($_REQUEST['requestername'] == "") || ($_REQUEST['address1'] == "") || ($_REQUEST['address2'] == "") || ($_REQUEST['requestercity'] == "") || ($_REQUEST['requesterstate'] == "") || ($_REQUEST['requesterzip'] == "") || ($_REQUEST['requesteremail'] == "") || ($_REQUEST['requestermobile'] == "") || ($_REQUEST['assigntech'] == "") || ($_REQUEST['inputdate'] == "")){
+   // msg displayed if required field missing
+   $msg = '<div class="alert alert-warning col-sm-6 ml-5 mt-2" role="alert"> Fill All Fileds </div>';
+  } else {
+    // Assigning User Values to Variable
+    $rid = $_REQUEST['request_id'];
+    $rinfo = $_REQUEST['request_info'];
+    $rdesc = $_REQUEST['requestdesc'];
+    $rname = $_REQUEST['requestername'];
+    $radd1 = $_REQUEST['address1'];
+    $radd2 = $_REQUEST['address2'];
+    $rcity = $_REQUEST['requestercity'];
+    $rstate = $_REQUEST['requesterstate'];
+    $rzip = $_REQUEST['requesterzip'];
+    $remail = $_REQUEST['requesteremail'];
+    $rmobile = $_REQUEST['requestermobile'];
+    $rassigntech = $_REQUEST['assigntech'];
+    $rdate = $_REQUEST['inputdate'];
+    $sql = "INSERT INTO assignwork_tb (request_id, request_info, request_desc, requester_name, requester_add1, requester_add2, requester_city, requester_state, requester_zip, requester_email, requester_mobile, assign_tech, assign_date) VALUES ('$rid', '$rinfo','$rdesc', '$rname', '$radd1', '$radd2', '$rcity', '$rstate', '$rzip', '$remail', '$rmobile', '$rassigntech', '$rdate')";
+    if($conn->query($sql) == TRUE){
+     // below msg display on form submit success
+     $msg = '<div class="alert alert-success col-sm-6 ml-5 mt-2" role="alert"> Work Assigned Successfully </div>';
+    } else {
+     // below msg display on form submit failed
+     $msg = '<div class="alert alert-danger col-sm-6 ml-5 mt-2" role="alert"> Unable to Assign Work </div>';
+    }
+  }
+  }
+ // Assign work Order Request Data going to submit and save on db assignwork_tb table [END]
+ ?>
+  <!-- Main Content area Start Last -->
+
+  <div class="container-xxl flex-grow-1 container-p-y">
+              <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Account Settings /</span> Account</h4>
+  <div class="card-body">
+                      <form id="formAccountSettings" method="POST" >
+                        <div class="row">
+                          <div class="mb-3 col-md-6">
+                            <label for="firstName" class="form-label">First Name</label>
+                            <input
+                              class="form-control"
+                              type="text"
+                              id="firstName"
+                              name="firstName"
+                              value="John"
+                              autofocus
+                            />
+                          </div>
+                          <div class="mb-3 col-md-6">
+                            <label for="lastName" class="form-label">Last Name</label>
+                            <input class="form-control" type="text" name="lastName" id="lastName" value="Doe" />
+                          </div>
+                          <div class="mb-3 col-md-6">
+                            <label for="email" class="form-label">E-mail</label>
+                            <input
+                              class="form-control"
+                              type="text"
+                              id="email"
+                              name="email"
+                              value="john.doe@example.com"
+                              placeholder="john.doe@example.com"
+                            />
+                          </div>
+                          <div class="mb-3 col-md-6">
+                            <label for="organization" class="form-label">Organization</label>
+                            <input
+                              type="text"
+                              class="form-control"
+                              id="organization"
+                              name="organization"
+                              value="ThemeSelection"
+                            />
+                          </div>
+                          <div class="mb-3 col-md-6">
+                            <label class="form-label" for="phoneNumber">Phone Number</label>
+                            <div class="input-group input-group-merge">
+                              <span class="input-group-text">US (+1)</span>
+                              <input
+                                type="text"
+                                id="phoneNumber"
+                                name="phoneNumber"
+                                class="form-control"
+                                placeholder="202 555 0111"
+                              />
+                            </div>
+                          </div>
+                          <div class="mb-3 col-md-6">
+                            <label for="address" class="form-label">Address</label>
+                            <input type="text" class="form-control" id="address" name="address" placeholder="Address" />
+                          </div>
+                          <div class="mb-3 col-md-6">
+                            <label for="state" class="form-label">State</label>
+                            <input class="form-control" type="text" id="state" name="state" placeholder="California" />
+                          </div>
+                          <div class="mb-3 col-md-6">
+                            <label for="zipCode" class="form-label">Zip Code</label>
+                            <input
+                              type="text"
+                              class="form-control"
+                              id="zipCode"
+                              name="zipCode"
+                              placeholder="231465"
+                              maxlength="6"
+                            />
+                          </div>
+                          <div class="mb-3 col-md-6">
+                            <label class="form-label" for="country">Country</label>
+                            <select id="country" class="select2 form-select">
+                              <option value="">Select</option>
+                              <option value="Australia">Australia</option>
+                              <option value="Bangladesh">Bangladesh</option>
+                              <option value="Belarus">Belarus</option>
+                              <option value="Brazil">Brazil</option>
+                              <option value="Canada">Canada</option>
+                              <option value="China">China</option>
+                              <option value="France">France</option>
+                              <option value="Germany">Germany</option>
+                              <option value="India">India</option>
+                              <option value="Indonesia">Indonesia</option>
+                              <option value="Israel">Israel</option>
+                              <option value="Italy">Italy</option>
+                              <option value="Japan">Japan</option>
+                              <option value="Korea">Korea, Republic of</option>
+                              <option value="Mexico">Mexico</option>
+                              <option value="Philippines">Philippines</option>
+                              <option value="Russia">Russian Federation</option>
+                              <option value="South Africa">South Africa</option>
+                              <option value="Thailand">Thailand</option>
+                              <option value="Turkey">Turkey</option>
+                              <option value="Ukraine">Ukraine</option>
+                              <option value="United Arab Emirates">United Arab Emirates</option>
+                              <option value="United Kingdom">United Kingdom</option>
+                              <option value="United States">United States</option>
+                            </select>
+                          </div>
+                          <div class="mb-3 col-md-6">
+                            <label for="language" class="form-label">Language</label>
+                            <select id="language" class="select2 form-select">
+                              <option value="">Select Language</option>
+                              <option value="en">English</option>
+                              <option value="fr">French</option>
+                              <option value="de">German</option>
+                              <option value="pt">Portuguese</option>
+                            </select>
+                          </div>
+                          <div class="mb-3 col-md-6">
+                            <label for="timeZones" class="form-label">Timezone</label>
+                            <select id="timeZones" class="select2 form-select">
+                              <option value="">Select Timezone</option>
+                              <option value="-12">(GMT-12:00) International Date Line West</option>
+                              <option value="-11">(GMT-11:00) Midway Island, Samoa</option>
+                              <option value="-10">(GMT-10:00) Hawaii</option>
+                              <option value="-9">(GMT-09:00) Alaska</option>
+                              <option value="-8">(GMT-08:00) Pacific Time (US & Canada)</option>
+                              <option value="-8">(GMT-08:00) Tijuana, Baja California</option>
+                              <option value="-7">(GMT-07:00) Arizona</option>
+                              <option value="-7">(GMT-07:00) Chihuahua, La Paz, Mazatlan</option>
+                              <option value="-7">(GMT-07:00) Mountain Time (US & Canada)</option>
+                              <option value="-6">(GMT-06:00) Central America</option>
+                              <option value="-6">(GMT-06:00) Central Time (US & Canada)</option>
+                              <option value="-6">(GMT-06:00) Guadalajara, Mexico City, Monterrey</option>
+                              <option value="-6">(GMT-06:00) Saskatchewan</option>
+                              <option value="-5">(GMT-05:00) Bogota, Lima, Quito, Rio Branco</option>
+                              <option value="-5">(GMT-05:00) Eastern Time (US & Canada)</option>
+                              <option value="-5">(GMT-05:00) Indiana (East)</option>
+                              <option value="-4">(GMT-04:00) Atlantic Time (Canada)</option>
+                              <option value="-4">(GMT-04:00) Caracas, La Paz</option>
+                            </select>
+                          </div>
+                          <div class="mb-3 col-md-6">
+                            <label for="currency" class="form-label">Currency</label>
+                            <select id="currency" class="select2 form-select">
+                              <option value="">Select Currency</option>
+                              <option value="usd">USD</option>
+                              <option value="euro">Euro</option>
+                              <option value="pound">Pound</option>
+                              <option value="bitcoin">Bitcoin</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div class="mt-2">
+                          <button type="submit" class="btn btn-primary me-2">Save changes</button>
+                          <button type="reset" class="btn btn-outline-secondary">Cancel</button>
+                        </div>
+                      </form>
+                    </div>
+
+</div>
+  <!-- below msg display if required fill missing or form submitted success or failed -->
+  <?php if(isset($msg)) {echo $msg; } ?>
+</div> <!-- End Row -->
+</div> <!-- End Container -->
+<!-- Only Number for input fields -->
+<script>
+  function isInputNumber(evt) {
+    var ch = String.fromCharCode(evt.which);
+    if (!(/[0-9]/.test(ch))) {
+      evt.preventDefault();
+    }
+  }
+</script>
