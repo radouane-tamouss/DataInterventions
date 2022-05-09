e96159ba23927043625a749eb7beb5ce5ea4d6ed
TAMOUSS Radouane <atamousse.red@gmail.com>
2022-05-09 12:13:13 +0100

    update check status page

--- Requester/CheckStatus.php
+++ Requester/CheckStatus.php
@@ -15,7 +15,17 @@ if($_SESSION['is_login']){
 <div class="layout-page">
   <!-- Navbar -->
 
+<style>
 
+@media print {
+	.no-printme  {
+		display: none;
+	}
+	.printme  {
+		display: block;
+	}
+}
+</style>
 
   <!-- / Navbar -->
 
@@ -27,15 +37,15 @@ if($_SESSION['is_login']){
       <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">vérifier l'état</h4>
 
       <!-- Basic Layout -->
-      <div class="row">
+      <div class="row ">
      
         <div class="col-xl">
           <div class="card mb-4">
            
-            <div class="card-body">
+            <div class="card-body no-printme">
               <form>
                 <div class="mb-3">
-                  <label class="form-label" for="basic-icon-default-fullname">Request ID</label>
+                  <label class="form-label" for="basic-icon-default-fullname">Demande ID</label>
                   <div class="input-group input-group-merge">
                    
                     <input
@@ -46,18 +56,21 @@ if($_SESSION['is_login']){
                       placeholder="Enter Request ID :"
                       aria-describedby="basic-icon-default-fullname2"
                       onkeypress="isInputNumber(event)"
+                      required
                     />
                   </div>
                 </div>
                 
                
-                <button type="submit" class="btn btn-primary" >Search</button>
+                <button type="submit" class="btn btn-primary" >Chercher</button>
                 
               </form>
 </div>
 </div>
 
-               
+          <div class="printme">
+
+            
               <?php
               if(isset($_REQUEST['checkid'])){
                 $sql = "SELECT * FROM assignwork_tb WHERE request_id = {$_REQUEST['checkid']}";
@@ -65,62 +78,62 @@ if($_SESSION['is_login']){
                 $row = $result->fetch_assoc();
                 if(($row['request_id']) == $_REQUEST['checkid']){ ?>
 
-      <h3 class="text-center mt-5">Assigned Work Details</h3>
+      <h3 class="text-center mt-5">détails de la demande</h3>
       <div class="printableTable">
       
      
           <table class="table table-bordered">
             <tbody>
               <tr>
-                <td>Request ID</td>
+                <td>Demande ID</td>
                 <td>
                   <?php if(isset($row['request_id'])) {echo $row['request_id']; } ?>
                 </td>
               </tr>
               <tr>
-                <td>Request Info</td>
+                <td>Demande Info</td>
                 <td>
                   <?php if(isset($row['request_info'])) {echo $row['request_info']; } ?>
                 </td>
               </tr>
               <tr>
-                <td>Request Description</td>
+                <td>Demande Description</td>
                 <td>
                   <?php if(isset($row['request_desc'])) {echo $row['request_desc']; } ?>
                 </td>
               </tr>
               <tr>
-                <td>Name</td>
+                <td>Nom</td>
                 <td>
                   <?php if(isset($row['requester_name'])) {echo $row['requester_name']; } ?>
                 </td>
               </tr>
               <tr>
-                <td>Address Line 1</td>
+                <td>Addresse 1</td>
                 <td>
                   <?php if(isset($row['requester_add1'])) {echo $row['requester_add1']; } ?>
                 </td>
               </tr>
               <tr>
-                <td>Address Line 2</td>
+                <td>Addresse  2</td>
                 <td>
                   <?php if(isset($row['requester_add2'])) {echo $row['requester_add2']; } ?>
                 </td>
               </tr>
               <tr>
-                <td>City</td>
+                <td>ville</td>
                 <td>
                   <?php if(isset($row['requester_city'])) {echo $row['requester_city']; } ?>
                 </td>
               </tr>
               <tr>
-                <td>State</td>
+                <td>Province</td>
                 <td>
                   <?php if(isset($row['requester_state'])) {echo $row['requester_state']; } ?>
                 </td>
               </tr>
               <tr>
-                <td>Pin Code</td>
+                <td>Code Postale</td>
                 <td>
                   <?php if(isset($row['requester_zip'])) {echo $row['requester_zip']; } ?>
                 </td>
@@ -132,29 +145,24 @@ if($_SESSION['is_login']){
                 </td>
               </tr>
               <tr>
-                <td>Mobile</td>
+                <td>Téléphone</td>
                 <td>
                   <?php if(isset($row['requester_mobile'])) {echo $row['requester_mobile']; } ?>
                 </td>
               </tr>
               <tr>
-                <td>Assigned Date</td>
+                <td>Date attribuée</td>
                 <td>
                   <?php if(isset($row['assign_date'])) {echo $row['assign_date']; } ?>
                 </td>
               </tr>
               <tr>
-                <td>Technician Name</td>
-                <td>Zahir Khan</td>
+                <td>nom du technicien</td>
+                <td>
+                  <?php if(isset($row['assign_tech'])) {echo $row['assign_tech']; } ?>
+                </td>
               </tr>
-              <tr>
-                <td>Customer Sign</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td>Technician Sign</td>
-                <td></td>
-              </tr>
+             
             </tbody>
           </table>
           </div>
@@ -184,14 +192,46 @@ if($_SESSION['is_login']){
           </div>
         </div>
       </div>
+        <!-- Footer -->
+        <footer class="content-footer footer bg-footer-theme">
+              <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
+                <div class="mb-2 mb-md-0">
+                  ©
+                  <script>
+                    document.write(new Date().getFullYear());
+                  </script>
+                  
+                  <a href="https://github.com/radouane-tamouss" target="_blank" class="footer-link fw-bolder">Dataxpress</a>
+                </div>
+                <div>
+                 
+
+                 
+
+                  <a
+                    href="mailto:atamousse.red@email.com"
+                    target="_blank"
+                    class="footer-link me-4"
+                    >Support</a
+                  >
+                </div>
+              </div>
+            </footer>
+          <!-- / Footer -->
+      </div> 
+      
     </div>
+
+    
     <!-- / Content -->
 
    
-
     <div class="content-backdrop fade"></div>
+    
   </div>
+  
   <!-- Content wrapper -->
+  
 </div>
 
 
