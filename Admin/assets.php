05fee67d620849e5f6ec3c66d86d58da055e9c84
TAMOUSS Radouane <atamousse.red@gmail.com>
2022-05-09 07:05:29 +0100

    update files

--- Admin/assets.php
+++ Admin/assets.php
@@ -12,17 +12,39 @@ session_start();
 ?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" rel="stylesheet">
 <div class="layout-page">
+<nav
+            class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
+            id="layout-navbar"
+          >
             <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
-              <a class="nav-item nav-link px-0 me-xl-4  m-2" href="javascript:void(0)">
+              <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
                 <i class="bx bx-menu bx-sm"></i>
               </a>
             </div>
+
+            <div class="m-1 my-4">
+             <img src="../images/data.png" alt="">
+            </div>
+
+
+            <div class="navbar-nav-right d-flex align-items-center" sid="navbar-collape">
+             
+
+              <ul class="navbar-nav flex-row align-items-center ms-auto">
+                <!-- Place this tag where you want the button to render. -->
+               
+
+                <a href="../logout.php">
+                <i class='bx bx-power-off ' style='color:#ff0a00' ></i>                        
+                      </a>
+              </ul>
+            </div>
+          </nav>
 
             <div class="content-wrapper">
 
              <!-- Content -->
              <div class="container-xxl flex-grow-1 container-p-y">
-              <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Tables /</span> Basic Tables</h4>
 
               <!-- Basic Bootstrap Table -->
               <div class="card">
