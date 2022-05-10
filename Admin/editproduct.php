8cd7d3982025f872155b43b7e9a17b30ba0cbd84
TAMOUSS Radouane <atamousse.red@gmail.com>
2022-05-10 13:21:59 +0100

    update edit asset page

--- Admin/editproduct.php
+++ Admin/editproduct.php
@@ -28,10 +28,10 @@ session_start();
   $sql = "UPDATE assets_tb SET pname = '$pname', pdop = '$pdop', pava = '$pava', ptotal = '$ptotal', poriginalcost = '$poriginalcost', psellingcost = '$psellingcost' WHERE pid = '$pid'";
     if($conn->query($sql) == TRUE){
      // below msg display on form submit success
-     $msg = '<div class="alert alert-success col-sm-12 mt-2" role="alert"> Updated Successfully </div>';
+     $msg = '<div class="alert alert-success col-sm-12 mt-2" role="alert">Modifier avec succes!</div>';
     } else {
      // below msg display on form submit failed
-     $msg = '<div class="alert alert-danger col-sm-6 ml-5 mt-2" role="alert"> Unable to Update </div>';
+     $msg = '<div class="alert alert-danger col-sm-6 ml-5 mt-2" role="alert">Impossible de modifier!</div>';
     }
   }
   }
@@ -48,7 +48,7 @@ session_start();
 
  <div class="container-xxl flex-grow-1 container-p-y">
 
- <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Assets /</span> Update Product Details</h4>
+ <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Produits /</span> Modifier</h4>
 
 <!-- Basic Layout -->
 <div class="row"> 
@@ -56,8 +56,7 @@ session_start();
 <div class="col-xl">
     <div class="card mb-4">
       <div class="card-header d-flex justify-content-between align-items-center">
-        <h5 class="mb-0">Update Product Details</h5>
-        <small class="text-muted float-end">Merged input group</small>
+        <h5 class="mb-0">Mettre à jour les détails du produit</h5>
       </div>
       <div class="card-body">
       <?php
@@ -71,7 +70,7 @@ session_start();
          
 
           <div class="row mb-3">
-                    <label class="col-sm-2 col-form-label" for="pid">Product ID</label>
+                    <label class="col-sm-2 col-form-label" for="pid">Produit ID</label>
                     <div class="col-sm-10">
                       <input
                       id="pid"
@@ -86,19 +85,19 @@ session_start();
 
          
           <div class="row mb-3">
-                    <label class="col-sm-2 col-form-label" for="pname">Name</label>
+                    <label class="col-sm-2 col-form-label" for="pname">Nom</label>
                     <div class="col-sm-10">
                       <input type="text" class="form-control today" id="pname" name="pname"  value="<?php if(isset($row['pname'])) {echo $row['pname']; }?>"/>
                     </div>
           </div>
           <div class="row mb-3">
-                    <label class="col-sm-2 col-form-label" for="pdop">DOP</label>
+                    <label class="col-sm-2 col-form-label" for="pdop">Date d'achat</label>
                     <div class="col-sm-10">
                       <input type="text" class="form-control" id="pdop" name="pdop"  value="<?php if(isset($row['pdop'])) {echo $row['pdop']; }?>"/>
                     </div>
           </div>
           <div class="row mb-3">
-                    <label class="col-sm-2 col-form-label" for="pava">Available</label>
+                    <label class="col-sm-2 col-form-label" for="pava">QT en Stock</label>
                     <div class="col-sm-10">
                       <input type="text" class="form-control" id="pava"  name="pava" value="<?php if(isset($row['pava'])) {echo $row['pava']; }?>" />
                     </div>
@@ -112,13 +111,13 @@ session_start();
                     </div>
           </div>
           <div class="row mb-3">
-                    <label class="col-sm-2 col-form-label" for="poriginalcost">Original Cost Each</label>
+                    <label class="col-sm-2 col-form-label" for="poriginalcost">prix d'achat</label>
                     <div class="col-sm-10">
                       <input type="text" class="form-control" id="poriginalcost"  onkeypress="isInputNumber(event)" name="poriginalcost" value="<?php if(isset($row['poriginalcost'])) {echo $row['poriginalcost']; }?>"/>
                     </div>
           </div>
           <div class="row mb-3">
-                    <label class="col-sm-2 col-form-label" for="psellingcost">Selling Price Each</label>
+                    <label class="col-sm-2 col-form-label" for="psellingcost">prix de vente</label>
                     <div class="col-sm-10">
                       <input type="text" class="form-control" id="psellingcost"  onkeypress="isInputNumber(event)" name="psellingcost" value="<?php if(isset($row['psellingcost'])) {echo $row['psellingcost']; }?>"/>
                     </div>
@@ -131,8 +130,8 @@ session_start();
         
          
           
-          <button type="submit" class=" btn btn-primary me-2 my-2" id="pupdate" name="pupdate">Update</button>
-          <a href="assets.php" class="btn btn-secondary">close</a>
+          <button type="submit" class=" btn btn-primary me-2 my-2" id="pupdate" name="pupdate">mettre à jour</button>
+          <a href="assets.php" class="btn btn-secondary">fermer</a>
 
           <?php if(isset($msg)) {echo $msg; } ?>
         </form>
