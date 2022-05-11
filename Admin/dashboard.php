94f4976a1f116f06def4b80629e9edbcd6907c4b
TAMOUSS Radouane <atamousse.red@gmail.com>
2022-05-11 10:28:55 +0100

    update files

--- Admin/dashboard.php
+++ Admin/dashboard.php
@@ -234,14 +234,14 @@ Cet espace est réservé aux membres du Conseil d’Administration et aux gestio
                                 <i class="bx bx-dots-vertical-rounded"></i>
                               </button>
                               <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt4">
-                                <a class="dropdown-item" href="request.php">Voir plus</a>
+                                <a class="dropdown-item" href="javascript:void(0);">View More</a>
                                
                               </div>
                             </div>
                           </div>
                           <span class="d-block mb-1">Clients</span>
                           <h3 class="card-title text-nowrap mb-2"><?php echo $customers ;?></h3>
-                          <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> <?php echo $customerstoday;?> Aujourdhui</small>
+                          <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> + <?php echo $customerstoday;?> Aujourdhui</small>
                         </div>
                       </div>
                     </div>
@@ -264,14 +264,14 @@ Cet espace est réservé aux membres du Conseil d’Administration et aux gestio
                                 <i class="bx bx-dots-vertical-rounded"></i>
                               </button>
                               <div class="dropdown-menu" aria-labelledby="cardOpt1">
-                                <a class="dropdown-item" href="technician.php">Voir plus</a>
+                                <a class="dropdown-item" href="javascript:void(0);">Voir plus</a>
                                 
                               </div>
                             </div>
                           </div>
                           <span class="fw-semibold d-block mb-1">Nb de technicien</span>
                           <h3 class="card-title mb-2">  <?php echo $totaltech; ?> </h3>
-                          <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> <?php echo $techtoday;?> Aujourdhui</small>
+                          <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> + <?php echo $techtoday;?> Aujourdhui</small>
                         </div>
                       </div>
                     </div>
