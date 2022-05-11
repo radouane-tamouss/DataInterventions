39c489fc5bd5d506b57d0739ac955133a96890d8
TAMOUSS Radouane <atamousse.red@gmail.com>
2022-05-11 10:32:11 +0100

    update files

--- Admin/work.php
+++ Admin/work.php
@@ -17,6 +17,34 @@ session_start();
           <!-- Navbar -->
 
         
+          <nav
+            class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
+            id="layout-navbar"
+          >
+            <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
+              <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
+                <i class="bx bx-menu bx-sm"></i>
+              </a>
+            </div>
+
+            <div class="m-1 my-4">
+             <img src="../images/data.png" alt="">
+            </div>
+
+
+            <div class="navbar-nav-right d-flex align-items-center" sid="navbar-collape">
+             
+
+              <ul class="navbar-nav flex-row align-items-center ms-auto">
+                <!-- Place this tag where you want the button to render. -->
+               
+
+                <a href="../logout.php">
+                <i class='bx bx-exit' style='color:#f70000'  ></i>
+                      </a>
+              </ul>
+            </div>
+          </nav>
             
             
           
@@ -24,7 +52,6 @@ session_start();
           <div class="content-wrapper">
             <!-- Content -->
             <div class="container-xxl flex-grow-1 container-p-y">
-              <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Interventions Infos</h4>
 
               <!-- Basic Bootstrap Table -->
               <div class="card">
