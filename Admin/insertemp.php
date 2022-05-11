d2df615103e9250f89291013ce34be88f5909051
TAMOUSS Radouane <atamousse.red@gmail.com>
2022-05-11 10:29:36 +0100

    update files

--- Admin/insertemp.php
+++ Admin/insertemp.php
@@ -13,7 +13,8 @@ if(isset($_REQUEST['empsubmit'])){
  // Checking for Empty Fields
  if(($_REQUEST['empName'] == "") || ($_REQUEST['empCity'] == "") || ($_REQUEST['empMobile'] == "") || ($_REQUEST['empEmail'] == "")){
   // msg displayed if required field missing
-  $msg = '<div class="alert alert-warning col-sm-12  mt-2" role="alert"> Fill All Fileds </div>';
+  $msg = '<div class="alert alert-warning col-sm-12  mt-2" role="alert">Remplir tous les champs
+  </div>';
  } else {
    // Assigning User Values to Variable
    $eName = $_REQUEST['empName'];
@@ -37,12 +38,39 @@ if(isset($_REQUEST['empsubmit'])){
 <div class="layout-page">
 
 
+<nav
+            class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
+            id="layout-navbar"
+          >
+            <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
+              <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
+                <i class="bx bx-menu bx-sm"></i>
+              </a>
+            </div>
+
+            <div class="m-1 my-4">
+             <img src="../images/data.png" alt="">
+            </div>
+
+
+            <div class="navbar-nav-right d-flex align-items-center" sid="navbar-collape">
+             
+
+              <ul class="navbar-nav flex-row align-items-center ms-auto">
+                <!-- Place this tag where you want the button to render. -->
+               
+
+                <a href="../logout.php">
+                <i class='bx bx-exit' style='color:#f70000'  ></i>
+                      </a>
+              </ul>
+            </div>
+          </nav>
 <div class="content-wrapper">
 
  <!-- Content -->
 
  <div class="container-xxl flex-grow-1 container-p-y">
-      <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Forms/</span> Vertical Layouts</h4>
 
       <!-- Basic Layout -->
       <div class="row">
@@ -50,37 +78,45 @@ if(isset($_REQUEST['empsubmit'])){
       <div class="col-xl">
           <div class="card mb-4">
             <div class="card-header d-flex justify-content-between align-items-center">
-              <h5 class="mb-0">Add New Technician</h5>
-              <small class="text-muted float-end">Merged input group</small>
+              <h5 class="mb-0">Ajouter un nouveau technicien</h5>
             </div>
             <div class="card-body">
               <form  class="mx-5" action="" method="POST">
                
 
                 <div class="row mb-3">
-                          <label class="col-sm-2 col-form-label" for="empName">Name</label>
+                          <label class="col-sm-2 col-form-label" for="empName">Nom</label>
                           <div class="col-sm-10">
                             <input
                             id="empName"
                             class="form-control form-control-lg"
                             type="text"
                             name="empName"
-                            placeholder="technician name"
+                            placeholder="nom de tech"
                             />
                           </div>
                 </div>
 
                
                 <div class="row mb-3">
-                          <label class="col-sm-2 col-form-label" for="empCity">City</label>
+                          <label class="col-sm-2 col-form-label" for="empCity">Ville</label>
                           <div class="col-sm-10">
-                            <input type="text" class="form-control today" id="empCity"  name="empCity"/>
+                            
+                            <select  class="form-select" id="empCity" name="empCity" >
+                              <?php
+                                $json = file_get_contents('../ville.json');
+                                $data = json_decode($json, true);
+                                  foreach($data['villes'] as $entry) {
+                                    echo "<option value=".$entry['ville']."select>".$entry['ville']."</option>";
+                                  }
+                                ?>
+                            </select>
                           </div>
                 </div>
                 <div class="row mb-3">
-                          <label class="col-sm-2 col-form-label" for="empMobile">Mobile</label>
+                          <label class="col-sm-2 col-form-label" for="empMobile">Telephone</label>
                           <div class="col-sm-10">
-                            <input type="text" class="form-control" id="empMobile" onkeypress="isInputNumber(event)" name="empMobile"/>
+                            <input type="text" class="form-control" id="empMobile" onkeypress="isInputNumber(event)" placeholder="num de tele" name="empMobile"/>
                           </div>
                 </div>
 
@@ -88,7 +124,7 @@ if(isset($_REQUEST['empsubmit'])){
                 <div class="row mb-3">
                           <label class="col-sm-2 col-form-label" for="empEmail">Email</label>
                           <div class="col-sm-10">
-                            <input type="email" class="form-control" id="empEmail"  name="empEmail"  />
+                            <input type="email" class="form-control" id="empEmail"  placeholder="name@email.com" name="empEmail"  />
                           </div>
                 </div>
                 
@@ -97,8 +133,8 @@ if(isset($_REQUEST['empsubmit'])){
               
                
                 
-                <button type="submit" class=" btn btn-primary me-2 my-2" id="empsubmit" name="empsubmit">Submit</button>
-                <a href="technician.php" class="btn btn-secondary">close</a>
+                <button type="submit" class=" btn btn-primary me-2 my-2" id="empsubmit" name="empsubmit">Ajouter</button>
+                <a href="technician.php" class="btn btn-secondary">Fermer</a>
 
                 <?php if(isset($msg)) {echo $msg; } ?>
               </form>
--- Admin/insertreq.php
+++ Admin/insertreq.php
@@ -13,7 +13,8 @@ if(isset($_REQUEST['reqsubmit'])){
  // Checking for Empty Fields
  if(($_REQUEST['r_name'] == "") || ($_REQUEST['r_email'] == "") || ($_REQUEST['r_password'] == "")){
   // msg displayed if required field missing
-  $msg = '<div class="alert alert-warning col-sm-12 mt-2" role="alert"> Fill All Fileds </div>';
+  $msg = '<div class="alert alert-warning col-sm-12 mt-2" role="alert">Remplir tous les champs
+  </div>';
  } else {
    // Assigning User Values to Variable
    $rname = $_REQUEST['r_name'];
@@ -22,10 +23,10 @@ if(isset($_REQUEST['reqsubmit'])){
    $sql = "INSERT INTO requesterlogin_tb (r_name, r_email, r_password) VALUES ('$rname', '$rEmail', '$rPassword')";
    if($conn->query($sql) == TRUE){
     // below msg display on form submit success
-    $msg = '<div class="alert alert-success col-sm-12 mt-2" role="alert"> Added Successfully </div>';
+    $msg = '<div class="alert alert-success col-sm-12 mt-2" role="alert"> Ajouter avec success!</div>';
    } else {
     // below msg display on form submit failed
-    $msg = '<div class="alert alert-danger col-sm-12 mt-2" role="alert"> Unable to Add </div>';
+    $msg = '<div class="alert alert-danger col-sm-12 mt-2" role="alert"> impossible d\'effectuer cette operation </div>';
    }
  }
  }
@@ -37,12 +38,39 @@ if(isset($_REQUEST['reqsubmit'])){
 <div class="layout-page">
 
 
+<nav
+            class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
+            id="layout-navbar"
+          >
+            <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
+              <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
+                <i class="bx bx-menu bx-sm"></i>
+              </a>
+            </div>
+
+            <div class="m-1 my-4">
+             <img src="../images/data.png" alt="">
+            </div>
+
+
+            <div class="navbar-nav-right d-flex align-items-center" sid="navbar-collape">
+             
+
+              <ul class="navbar-nav flex-row align-items-center ms-auto">
+                <!-- Place this tag where you want the button to render. -->
+               
+
+                <a href="../logout.php">
+                <i class='bx bx-exit' style='color:#f70000'  ></i>
+                      </a>
+              </ul>
+            </div>
+          </nav>
 <div class="content-wrapper">
 
  <!-- Content -->
 
  <div class="container-xxl flex-grow-1 col-lg-8 container-p-y">
-      <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Forms/</span> Vertical Layouts</h4>
 
       <!-- Basic Layout -->
       <div class="row">
@@ -50,7 +78,7 @@ if(isset($_REQUEST['reqsubmit'])){
       <div class="col-xl">
           <div class="card mb-4">
             <div class="card-header d-flex justify-content-between align-items-center">
-              <h5 class="mb-0">Add New Requester</h5>
+              <h5 class="mb-0">Ajouter un nouveu demendeur</h5>
               <small class="text-muted float-end">Merged input group</small>
             </div>
             <div class="card-body">
@@ -58,7 +86,7 @@ if(isset($_REQUEST['reqsubmit'])){
                
 
                 <div class="row mb-3">
-                          <label class="col-sm-2 col-form-label" for="r_name">Name</label>
+                          <label class="col-sm-2 col-form-label" for="r_name">Nom</label>
                           <div class="col-sm-10">
                             <input
                             id="r_name"
@@ -80,7 +108,7 @@ if(isset($_REQUEST['reqsubmit'])){
                 
                
                 <div class="row mb-3">
-                          <label class="col-sm-2 col-form-label" for="r_password">Password</label>
+                          <label class="col-sm-2 col-form-label" for="r_password">Mot de pass</label>
                           <div class="col-sm-10">
                             <input type="password" class="form-control today" id="r_password"  name="r_password"/>
                           </div>
@@ -93,8 +121,8 @@ if(isset($_REQUEST['reqsubmit'])){
               
                
                 
-                <button type="submit" class=" btn btn-primary me-2 my-2" id="reqsubmit" name="reqsubmit">Submit</button>
-                <a href="requester.php" class="btn btn-secondary">close</a>
+                <button type="submit" class=" btn btn-primary me-2 my-2" id="reqsubmit" name="reqsubmit">Ajouter</button>
+                <a href="requester.php" class="btn btn-secondary">Fermer</a>
 
                 <?php if(isset($msg)) {echo $msg; } ?>
               </form>
