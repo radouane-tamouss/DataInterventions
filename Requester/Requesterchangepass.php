7b580f6564b42459b804240f6b750dd6368fe272
TAMOUSS Radouane <atamousse.red@gmail.com>
2022-05-12 08:44:27 +0100

    update files

--- Requester/Requesterchangepass.php
+++ Requester/Requesterchangepass.php
@@ -1,9 +1,9 @@
 <?php
 define('TITLE', 'Change Password');
 define('PAGE', 'Requesterchangepass');
+session_start();
 include('includes/header.php');
 include('../dbConnection.php');
-session_start();
 if($_SESSION['is_login']){
  $rEmail = $_SESSION['rEmail'];
 } else {
