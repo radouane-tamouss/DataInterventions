25a599bbf3b50d2e027f0256365ae0996f20ae7a
TAMOUSS Radouane <atamousse.red@gmail.com>
2022-05-21 08:51:41 +0100

    update submit request page

--- Requester/submitrequestsuccess.php
+++ Requester/submitrequestsuccess.php
@@ -12,35 +12,45 @@ $sql = "SELECT * FROM submitrequest_tb WHERE request_id = {$_SESSION['myid']}";
 $result = $conn->query($sql);
 if($result->num_rows == 1){
  $row = $result->fetch_assoc();
- echo "<div class='ml-5 mt-5'>
+ echo "
+  <div class='layout-page'>
+ <div class='content-wrapper'>
+  <div class='container-xxl flex-grow-1 container-p-y'>
+ <div class='ml-5 mt-5'>
  <table class='table'>
   <tbody>
    <tr>
-     <th>Request ID</th>
+     <th>Demande ID</th>
      <td>".$row['request_id']."</td>
    </tr>
    <tr>
-     <th>Name</th>
+     <th>Nom</th>
      <td>".$row['requester_name']."</td>
    </tr>
    <tr>
-   <th>Email ID</th>
+   <th>Email</th>
    <td>".$row['requester_email']."</td>
   </tr>
    <tr>
-    <th>Request Info</th>
+    <th>Demande Info</th>
     <td>".$row['request_info']."</td>
    </tr>
    <tr>
-    <th>Request Description</th>
+    <th>Discription de la Demande</th>
     <td>".$row['request_desc']."</td>
    </tr>
 
-   <tr>
-    <td><form class='d-print-none'><input class='btn btn-danger' type='submit' value='Print' onClick='window.print()'></form></td>
-  </tr>
   </tbody>
  </table> </div>
+ 
+  <div class='text-center' style='padding:20px'>
+            <form class='d-print-none d-inline mr-3'><input class='btn btn-primary' type='submit' value='Print' onClick='window.print()'></form>
+            
+            
+
+            <form class='d-print-none d-inline' action='../admin/work.php'><input class='btn btn-secondary' type='submit' value='Close'></form>
+            
+          </div>
  ";
 
 
@@ -48,7 +58,8 @@ if($result->num_rows == 1){
   echo "Failed";
 }
 ?>
-
+</div>
+</div>
 
 <?php
 include('includes/footer.php'); 
